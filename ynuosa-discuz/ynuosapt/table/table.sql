e650a35cc9d75a4508827718faf878b69364f77a
STRRL <str_ruiling@outlook.com>
2017-07-22 19:59:12 +0800

    delete uesless file

--- ynuosa-discuz/ynuosapt/table/table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `pre_ynuosapt_tid_files` (
-  `lid` int(11) NOT NULL AUTO_INCREMENT,
-  `tid` int(11) DEFAULT NULL,
-  `downloadcount` int(11) DEFAULT '0',
-  `hash_info` varchar(40) DEFAULT NULL,
-  `uploadtime` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`lid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
